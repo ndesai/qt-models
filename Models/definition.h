ab3a7a7133948e33f0dd70c1b250de99d7546884
Niraj Desai <niraj@app.st>
2020-12-24 17:30:23 -0500

    Use AppDataLocation

--- Models/definition.h
+++ Models/definition.h
@@ -4,7 +4,7 @@
 #include <QDir>
 #define DEBUG if(0) qDebug() << __PRETTY_FUNCTION__
 #ifdef Q_OS_IOS
-#define BASEPATH QString(QStandardPaths::standardLocations(QStandardPaths::DataLocation).first() + QDir::separator())
+#define BASEPATH QString(QStandardPaths::standardLocations(QStandardPaths::AppDataLocation).first() + QDir::separator())
 #else
 #define BASEPATH QString(QDir::homePath() + QDir::separator())
 #endif
